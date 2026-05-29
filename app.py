4e6bc7c0e6a1b9b81e07a06abadcbb8ba0ddc26e
noyla123 <aestetoqnoyla@gmail.com>
2026-05-29 23:34:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 from flask import Flask, render_template, request
+import os
 
-application = Flask(__name__)
+app = Flask(__name__)
 
-@application.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
    if request.method == 'POST':
       namaDepan = request.form['namaDepan']
       namaBelakang = request.form['namaBelakang']
+
       nama = '%s %s' % (namaDepan, namaBelakang)
-      p=nama
-      C = ' '
-      k=3
+
+      p = nama
+      C = ''
+      k = 3
+
       for i in range(len(p)):
          c = chr(ord(p[i]) + k)
          C = C + c
-      return render_template('response.html', nama=C)
-   return render_template('form.html')
 
-import os
+      return render_template('response.html', nama=C)
+
+   return render_template('form.html')
 
 if __name__ == '__main__':
    port = int(os.environ.get("PORT", 5000))
-   application.run(host='0.0.0.0', port=port)
+   app.run(host='0.0.0.0', port=port)
